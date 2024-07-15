fd48300b72561805b321436dfbe636db3b3f58cf
sahidDev09 <iamsahidofficial99@gmail.com>
2024-07-15 23:17:31 +0600

    init projects

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className=" text-5xl font-bold text-center">Hello world</h1>
+      </div>
+      
+    
+    </>
+  )
+}
+
+export default App
